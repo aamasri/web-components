f6d1877c1b997424313af8d07fee3e7dc76e0729
ananda <ananda@auroraweb.ca>
2024-04-23 19:28:40 -0600

    Release version 1.2.4 - Upgraded Search component with onchange detail and a clear search button.

--- src/svelte/components/Search.svelte
+++ src/svelte/components/Search.svelte
@@ -8,24 +8,33 @@
     const dispatch = createEventDispatcher();
     const dispatchChange = () => {
         window.clearTimeout(debounceDelay);
-        debounceDelay = window.setTimeout(() => dispatch('change'), 800);
+        debounceDelay = window.setTimeout(() => dispatch('change', searchString), 800);
+    }
+
+    function clearSearch() {
+        searchString = '';
+        dispatch('change', searchString);
     }
 </script>
 
 
 
 <div class="search input-group">
-    <span class="input-group-text"><i class="fas fa-search"></i></span>
+    <span class="input-group-text"><i class="fa-solid fa-search"></i></span>
     <input type="text"
-            class="form-control"
-            placeholder="{placeholder}"
-            bind:value="{searchString}"
-            on:input={dispatchChange} >
+           class="form-control"
+           placeholder="{placeholder}"
+           bind:value="{searchString}"
+           on:input={dispatchChange} >
+
+    {#if searchString}
+        <button class="input-group-text" on:click={clearSearch}><i class="fa-solid fa-xmark"></i></button>
+    {/if}
 </div>
 
 
 
 <style lang="stylus" global>
-    .search
-        max-width 20em
+  .search
+    max-width 20em
 </style>
